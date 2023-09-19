c7ef3382ba741aa192048127f3ef54d44fd9adfe
AyTee001 <tiutiunanastasia@gmail.com>
2023-09-19 11:28:37 +0300

    Create program

---
+++ PalindromeChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Palindroms
+{
+    public class PalindromeChecker
+    {
+        private readonly string _word;
+
+        public PalindromeChecker(string word)
+        {
+            _word = word;
+        }
+
+        public bool IsWordPalindrom()
+        {
+            string clearedWord = Regex.Replace(_word, @"[\s\p{P}]+", "").ToLower();
+
+            if (string.IsNullOrEmpty(clearedWord))
+            {
+                return false;
+            }
+
+            return clearedWord.Equals(ReverseString(clearedWord));
+        }
+
+        private string ReverseString(string stringToReverse)
+        {
+            var charArray = stringToReverse.ToCharArray();
+            Array.Reverse(charArray);
+
+            return new string(charArray);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Palindroms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Please, give me your input and I will check it for being a palindrom");
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("You entered nothing");
+                return;
+            }
+
+            var checker = new PalindromeChecker(input);
+            if (checker.IsWordPalindrom())
+            {
+                Console.WriteLine("That was a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("That was not a palindrome");
+            }
+        }
+    }
+}
